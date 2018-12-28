9e4e046ebba19e3b68bc91bcd49e5d113b3f19f5
YuebanZhou <598065612@qq.com>
2018-12-28 14:41:31 +0800

    201812281441

--- js/main.js
+++ js/main.js
@@ -1,43 +1,18 @@
 $(document).ready(function () {
+  // 设定banner高度
   var contentHeight = $(window).height() - $("#head").height();
-  $("#content").height(contentHeight)
-  $("#content .contentBox").height(contentHeight)
-  $("#content .contentBox .bannerBox").height(contentHeight)
-  $("#content .contentBox .bannerBox ul").height(contentHeight)
+  $("#content").height(contentHeight);
+  $("#content .contentBox").height(contentHeight);
+  $("#content .contentBox .bannerBox").height(contentHeight);
+  $("#content .contentBox .bannerBox ul").height(contentHeight);
   // $("#content .contentBox .bannerBox ul li").height(contentHeight)
-  $("#content .contentBox .bannerBox span").height(contentHeight)
-  
-  // 切换tab栏效果
-  tabClick();
-  // 渲染banner内容
-  rebanner();
-  // 左右切换banner
-  changeimg();
-  // 渲染experience内容
-  reexperience()
-});
-// 切换tab栏效果
-function tabClick() {
-  // 切换tab
-  $("a").on("click", function () {
-    $("li").removeClass("now");
-    $(this)
-      .parent()
-      .addClass("now");
-    $("#content")
-      .find(".contentBox")
-      .hide(500);
-    var index = $(".now").index();
-    $("#content")
-      .find(".contentBox")
-      .eq(index)
-      .show(500);
-  });
-}
-// 渲染banner内容
-function rebanner() {
-  var imgarr;
-  var msgarr;
+  $("#content .contentBox .bannerBox span").height(contentHeight);
+  var imgarr = [];
+  var msgarr = [];
+  var exparr = [];
+  var skillarr = [];
+  var contactarr = [];
+  var endarr = [];
   $.ajax({
     url: "./json/msg.json",
     type: "post",
@@ -46,8 +21,33 @@ function rebanner() {
     success: function (data) {
       imgarr = data.imgarr;
       msgarr = data.msgarr;
+      exparr = data.exparr;
+      skillarr = data.skillarr;
+      contactarr = data.contactarr;
+      endarr = data.endarr;
     }
-  })
+  });
+  // 切换tab栏效果
+  tabClick();
+  rebanner(imgarr, msgarr);
+  reexperience(exparr);
+  reinterest(skillarr);
+  recontact(contactarr);
+  reend(endarr);
+});
+// 切换tab栏效果
+function tabClick() {
+  // 切换tab
+  $("#head li").on("click", function () {
+    $("li").removeClass("now");
+    $(this).addClass("now");
+    $("#content").find(".contentBox").hide(500);
+    var index = $(this).index();
+    $("#content").find(".contentBox").eq(index).show(500);
+  });
+}
+// 渲染banner内容
+function rebanner(imgarr, msgarr) {
   // 渲染banner
   var str = "";
   var len = imgarr.length;
@@ -70,9 +70,7 @@ function rebanner() {
   var basicWidth = $(".bannerBox").width();
   $("#content .banner .bannerBox ul li").width(basicWidth);
   $("#content .banner .bannerBox ul").width(basicWidth * (len + 2));
-}
-// 左右切换banner
-function changeimg() {
+
   var liW = $("#content .bannerBox ul li").width();
   var len = $("#content .bannerBox ul li").length;
   // 左右点击
@@ -94,19 +92,10 @@ function changeimg() {
     }
     $("#content ul").css("left", -liW * i);
   });
+
 }
 // 渲染experience内容
-function reexperience() {
-  var exparr = [];
-  $.ajax({
-    url: "./json/msg.json",
-    type: "post",
-    dataType: "json",
-    async: false,
-    success: function (data) {
-      exparr = data.exparr;
-    }
-  });
+function reexperience(exparr) {
   var str = ""
   for (var i = 0; i < exparr.length; i++) {
     str += "<div class='block'><p class='title'><span class='time'>" + exparr[i].time + "</span>"
@@ -126,11 +115,79 @@ function reexperience() {
   }
 
   $(".experienceBox .boxcontain").html(str);
-  $(".experienceBox .boxcontain .block").on("mouseenter",function(){
+  $(".experienceBox .boxcontain .block").on("mouseenter", function () {
     $(this).find("i").addClass("lime")
   })
-  $(".experienceBox .boxcontain .block").on("mouseleave",function(){
+  $(".experienceBox .boxcontain .block").on("mouseleave", function () {
     $(this).find("i").removeClass("lime")
   })
-  
+
+}
+// 渲染interest内容
+function reinterest(skillarr) {
+  var str = "";
+  for (var i = 0; i < skillarr.length; i++) {
+    str += `
+        <li>
+          <div class="frount">
+              <div class="behind" data-percent="` + skillarr[i].percent + `"></div>
+          </div>
+          <p>` + skillarr[i].skillName + `</p>
+        </li>
+        `
+  }
+  $("#content .interest ul").html(str)
+  $("#content .interest .behind").each(function () {
+    var percent = parseInt($(this).attr("data-percent"));
+    $(this).css({
+      "top": 180 - percent * 1.8
+    });
+    if (percent <= 20) {
+      $(this).css({
+        "background": "#EB524D"
+      })
+    } else if (percent > 20 && percent <= 40) {
+      $(this).css({
+        "background": "#F07643"
+      })
+    } else if (percent > 40 && percent <= 60) {
+      $(this).css({
+        "background": "#F9BE0A"
+      })
+    } else if (percent > 60 && percent <= 80) {
+      $(this).css({
+        "background": "#CDCD47"
+      })
+    } else if (percent > 80 && percent <= 100) {
+      $(this).css({
+        "background": "#748950"
+      })
+    }
+  })
+}
+// 渲染contact内容
+function recontact(contactarr) {
+  var str = "";
+  for (var i = 0; i < contactarr.length; i++) {
+    str += `
+    <div class="cblock">
+      <div class="top" style="color:#ccc"><i class="iconfont iconfont3 icon-` + contactarr[i].name + `"></i><br><span>` + contactarr[i].num + `</span></div>
+      <div class="bottom"><img src="` + contactarr[i].img + `" alt=""></div>
+    </div>
+    `
+  };
+  $("#content .contactBox").html(str);
+
+}
+// 渲染end内容
+function reend(endarr) {
+  var str = "";
+  for (var i = 0; i < endarr.length; i++) {
+    str += `
+    <a href="`+endarr[i].src+`" title="`+endarr[i].name+`" target="_blank">
+      <i class="iconfont iconfont4 icon-`+endarr[i].name+`"></i>
+    </a>
+    `
+  };
+  $("#content .endBox div").html(str)
 }
